7a7d17d828feb23a6a20bfd82bc3bc42b5926ad8
Ben Senica <senicaben@gmail.com>
2019-08-02 15:06:48 +0200

    added code to validate password to requirements

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ function submitForm() {
     var uPhone = document.forms["register"]["phone"].value;
     var uAge = document.forms["register"]["age"].value;
     var uHobbies = document.forms["register"]["hobbies"].value;
-    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/;
 
     if (uPassword !== uConfirmPassword) {
         alert("Passwords don't match!");
         return false;
     } else if (uPassword.match(passw)) {
         window.location.replace("success.html");
+        return false;
     } else {
         alert("Password must contain at least 1 number and 1 uppercase!");
         return false;

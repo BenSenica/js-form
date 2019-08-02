f0d313a696637ee735202acf6efc85669bea8555
Ben Senica <senicaben@gmail.com>
2019-08-02 15:36:21 +0200

    beter validation for pw

--- script.js
+++ script.js
@@ -13,14 +13,16 @@ function submitForm() {
     var uHobbies = document.forms["register"]["hobbies"].value;
     var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/;
 
+
     if (uPassword !== uConfirmPassword) {
         alert("Passwords don't match!");
         return false;
-    } else if (uPassword.match(passw)) {
-        window.location.replace("success.html");
+    } else if (!uPassword.match(passw)) {
+
+        alert("Password must contain at least 1 number and 1 uppercase!");
         return false;
     } else {
-        alert("Password must contain at least 1 number and 1 uppercase!");
+        window.location.replace("success.html");
         return false;
     }
 

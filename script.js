40d9b5545d51e8bb5bdc9a53108a839adf02cd03
Ben Senica <senicaben@gmail.com>
2019-08-02 16:52:21 +0200

    completed challenge 4 with added focus and select on error input fields

--- script.js
+++ script.js
@@ -11,18 +11,26 @@ function submitForm() {
     var uPhone = document.forms["register"]["phone"].value;
     var uAge = document.forms["register"]["age"].value;
     var uHobbies = document.forms["register"]["hobbies"].value;
-    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/;
-
+    var passw = /^(?=.*\d)(?=.*[A-Z])/;
+    var emailAddress = /^(?:\S+@gmail\.com )*\S+@gmail\.com$/;
 
     if (uPassword !== uConfirmPassword) {
+        document.getElementById("pword").focus();
+        document.getElementById("pword").select();
         alert("Passwords don't match!");
         return false;
     } else if (!uPassword.match(passw)) {
-
+        document.getElementById("pword").focus();
+        document.getElementById("pword").select();
         alert("Password must contain at least 1 number and 1 uppercase!");
         return false;
+    } else if (!uEmail.match(emailAddress)) {
+        document.getElementById("gmail").focus();
+        document.getElementById("gmail").select();
+        alert("Please register with a gmail account!");
+        return false;
     } else {
-        window.location.replace("success.html");
+        location.href = "success.html";
         return false;
     }
 

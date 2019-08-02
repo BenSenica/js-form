89f51207c68d4a41be48d85c5df98c97a3417285
Ben Senica <senicaben@gmail.com>
2019-08-02 11:42:46 +0200

    finished challa=enge 2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-var uFirstName = document.getElementsByName("firstName").value;
+function submitForm() {
+    var uFirstName = document.forms["register"]["firstName"].value;
+    var uLastName = document.forms["register"]["lastName"].value;
+    var uName = document.forms["register"]["userName"].value;
+    var uEmail = document.forms["register"]["email"].value;
+    var uPassword = document.forms["register"]["password"].value;
+    var uConfirmPassword = document.forms["register"]["confirmPassword"].value;
+    var uAddress = document.forms["register"]["address"].value;
+    var uCity = document.forms["register"]["city"].value;
+    var uZip = document.forms["register"]["zip"].value;
+    var uPhone = document.forms["register"]["phone"].value;
+    var uAge = document.forms["register"]["age"].value;
+    var uHobbies = document.forms["register"]["hobbies"].value;
 
-console.log(uFirstName);
+}
